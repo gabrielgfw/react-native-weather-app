d8a78e3d84990ab29a916ed8ec5ac1828eff2e3e
Gabriel Felipe Werner <gabriel_gfw@hotmail.com>
2021-02-01 14:14:27 -0300

    add request to weather icon, simple style add to show up the image

--- components/WeatherInfo.js
+++ components/WeatherInfo.js
@@ -1,11 +1,19 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, Image } from 'react-native'
 
 export default function WeatherInfo({ currentWeather }) {
-    const { main: {temp} } = currentWeather;
+    const { 
+        main: { temp },
+        weather: [ details ]
+    } = currentWeather;
+
+    const { icon } = details;
+    const iconUrl = `https://openweathermap.org/img/wn/${icon}@4x.png`;
+
 
     return (
         <View style={styles.weatherInfo}>
+            <Image style={styles.weatherIcon} source={{ uri: iconUrl }}></Image>
             <Text>{temp}</Text>
         </View>
     );
@@ -16,4 +24,9 @@ const styles = StyleSheet.create({
     weatherInfo: {
         alignItems: "center",
     },
+
+    weatherIcon: {
+        width: 100,
+        height: 100,        
+    }, 
 });
